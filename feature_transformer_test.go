4d3eb1ea21d448b7d147928dba46f2765f67773c
David Cuadrado <dcuadrado@truora.com>
2020-12-29 10:29:24 -0500

    Fix and verify feature transformer

--- feature_transformer_test.go
+++ feature_transformer_test.go
@@ -9,11 +9,9 @@ import (
 )
 
 func TestFeatureTransformer(t *testing.T) {
-	g := NewGraph()
-
 	testCases := []struct {
 		desc              string
-		input             *Node
+		input             tensor.Tensor
 		vbs               int
 		independentBlocks int
 		output            int
@@ -23,34 +21,34 @@ func TestFeatureTransformer(t *testing.T) {
 	}{
 		{
 			desc: "",
-			input: NewTensor(g, tensor.Float64, 2, WithShape(6, 2), WithName("input"), WithValue(
-				tensor.New(
-					tensor.WithShape(6, 2),
-					tensor.WithBacking([]float64{0.4, 1.4, 2.4, 3.4, 4.4, 5.4, 6.4, 7.4, 8.4, 9.4, 10.4, 11.4}),
-				),
-			)),
+			input: tensor.New(
+				tensor.WithShape(6, 2),
+				tensor.WithBacking([]float64{0.4, 1.4, 2.4, 3.4, 4.4, 5.4, 6.4, 7.4, 8.4, 9.4, 10.4, 11.4}),
+			),
 			vbs:               2,
 			output:            2,
 			independentBlocks: 5,
 			expectedShape:     tensor.Shape{6, 2},
-			expectedOutput:    []float64{-0.4395035730343544, -0.28961441959885387, 3.3431244714105195, 0.7463088298910071, 0.8543397630255111, 0.33027296796752753, 4.136399173999854, 0.976768932989377, 2.0747412532576526, 0.995551329231948, 4.497775695459858, 1.494460903074312},
+			expectedOutput:    []float64{-0.46379327241699114, -0.2870165771203542, 1.87719497530188, 2.053971670598517, 0.24331350876955662, 0.42009020406619374, 2.5843017564884283, 2.761078451785065, 0.9504202899561044, 1.1271969852527415, 3.291408537674975, 3.468185232971612},
 		},
 	}
-
-	tn := &Model{g: g}
 
 	for _, tcase := range testCases {
 		t.Run(tcase.desc, func(t *testing.T) {
 			c := require.New(t)
 
+			g := NewGraph()
+			tn := &Model{g: g}
+
+			input := NewTensor(g, tensor.Float64, tcase.input.Dims(), WithShape(tcase.input.Shape()...), WithName("input"), WithValue(tcase.input))
 			priors := NewTensor(g, Float64, tcase.input.Dims(), WithShape(tcase.input.Shape()...), WithInit(Ones()))
 			x, err := tn.FeatureTransformer(FeatureTransformerOpts{
 				VirtualBatchSize:  tcase.vbs,
-				OutputDimension:    tcase.output,
+				OutputDimension:   tcase.output,
 				Shared:            nil,
 				IndependentBlocks: tcase.independentBlocks,
 				WeightsInit:       initDummyWeights,
-			})(tcase.input, priors)
+			})(input, priors)
 
 			if tcase.expectedErr != "" {
 				c.Error(err)
