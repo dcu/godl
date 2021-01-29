89d05688a9ebb540873bcd59b0c3aa554b4e9686
David Cuadrado <dcuadrado@truora.com>
2021-01-29 15:14:57 -0500

    Implement cross entropy loss function

--- losses.go
+++ losses.go
@@ -19,3 +19,20 @@ func MSELoss(output *gorgonia.Node, target *gorgonia.Node, opts MSELossOpts) *go
 
 	return gorgonia.Must(reduction(gorgonia.Must(gorgonia.Square(sub))))
 }
+
+type CrossEntropyLossOpt struct {
+	Reduction string
+}
+
+// CrossEntropyLoss implements cross entropy loss function
+func CrossEntropyLoss(output *gorgonia.Node, target *gorgonia.Node, opts CrossEntropyLossOpt) *gorgonia.Node {
+	reduction := gorgonia.Mean
+	if opts.Reduction == "sum" {
+		reduction = gorgonia.Sum
+	}
+
+	output = gorgonia.Must(Softmax(output))
+	cost := gorgonia.Must(gorgonia.HadamardProd(gorgonia.Must(gorgonia.Neg(gorgonia.Must(gorgonia.Log(output)))), target))
+
+	return gorgonia.Must(reduction(cost))
+}
